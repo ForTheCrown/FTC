33a83757802a842d529c7108782e862cd65030b2
BotulToxin <73311304+JulieWoolie@users.noreply.github.com>
2023-02-14 22:50:07 +0200

    Finish donator display update script

--- src/main/resources/scripts/donator_display/update.js
+++ src/main/resources/scripts/donator_display/update.js
@@ -1,4 +1,5 @@
 import "@bukkit.entity.ArmorStand";
+import "@bukkit.inventory.ItemStack";
 
 if (args.length < 1) {
   throw "Expected args[0] to be player name/uuid";
@@ -9,7 +10,7 @@ if (args.length < 2) {
 }
 
 const user = Users.get(args[0]);
-color COLOR = NamedTextColor.NAMES.valueOrThrow(args[1]);
+const COLOR = NamedTextColor.NAMES.valueOrThrow(args[1]);
 
 if (user == null) {
   throw "Unknown user " + args[0];
@@ -24,8 +25,9 @@ const standLocations = [
   { x: 211.5, y: 73.5, z: 207.5 },
   { x: 209.5, y: 72.5, z: 208.5 },
   { x: 209.5, y: 73.5, z: 210.5 },
-  { x: 207.5, y: 73.5, z: 211.5 },
-]
+  { x: 207.5, y: 72.5, z: 211.5 },
+];
+const armorStands = createArmorStandArray();
 
 if (Cooldown.containsOrAdd(user, "donator_display", 15 * 20)) {
   logger.warn("Player {} is on cooldown, cannot update donator display", user);
@@ -34,10 +36,12 @@ if (Cooldown.containsOrAdd(user, "donator_display", 15 * 20)) {
 }
 
 function updateDisplays() {
-  const armorStands = createArmorStandArray();
-
   const displayName = user.getTabName().color(COLOR);
   const profile = user.getOfflinePlayer().getPlayerProfile();
+  
+  if (!profile.isComplete()) {
+    profile.complete(true, true);
+  }
 
   let headBuilder = ItemStacks.headBuilder();
   headBuilder.setProfile(profile);
@@ -46,6 +50,7 @@ function updateDisplays() {
 
   const newFirst = armorStands[0];
   newFirst.customName(displayName);
+  newFirst.setCustomNameVisible(true);
   newFirst.getEquipment().setHelmet(headBuilder.build());
   giveArmor(newFirst);
 }
@@ -60,13 +65,11 @@ function pushStandsBack() {
     const sEquip = stand.getEquipment();
     const nEquip = next.getEquipment();
 
-    if (ItemStacks.isEmpty(nEquip.getHelmet())) {
-      sEquip.setHelmet(null);
-      sEquip.setLeggings(null);
-      sEquip.setBoots(null);
-    }
-
     nEquip.setHelmet(sEquip.getHelmet());
+
+    if (ItemStacks.notEmpty(sEquip.getHelmet())) {
+      next.setCustomNameVisible(true);
+    }
 
     if (ItemStacks.notEmpty(sEquip.getHelmet())) {
       giveArmor(next);
@@ -76,7 +79,7 @@ function pushStandsBack() {
 
 function giveArmor(stand) {
   let eq = stand.getEquipment();
-  eq.setChestPlace(new ItemStack(Material.GOLD_CHESTPLATE));
+  eq.setChestplate(new ItemStack(Material.GOLDEN_CHESTPLATE));
   eq.setLeggings(new ItemStack(Material.IRON_LEGGINGS));
   eq.setBoots(new ItemStack(Material.GOLDEN_BOOTS));
 }
