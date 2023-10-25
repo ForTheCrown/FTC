b87d42eb005980fc03ccd9a80d5d719e48e7f549
BotulToxin <73311304+JulieWoolie@users.noreply.github.com>
2023-10-25 17:13:18 +0300

    Add per-player entity display name rendering

--- commons/src/main/java/net/forthecrown/packet/PacketListeners.java
+++ commons/src/main/java/net/forthecrown/packet/PacketListeners.java
@@ -2,12 +2,30 @@ package net.forthecrown.packet;
 
 import net.forthecrown.BukkitServices;
 import net.forthecrown.registry.Registry;
+import net.kyori.adventure.text.Component;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.TextDisplay;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public interface PacketListeners {
 
   static PacketListeners listeners() {
     return BukkitServices.load(PacketListeners.class).orElseGet(NopListeners::nop);
   }
+
+  /**
+   * Sets the 'display text' a specified player will see for the specified entity.
+   * <p>
+   * "display text" means, for most entities, their customName field. Except for {@link TextDisplay}
+   * entities, they will have their {@link TextDisplay#text(Component)} field changed.
+   *
+   * @param entity Entity to change the display text of
+   * @param viewer Player viewing the entity
+   * @param text Text to change it to, or {@code null}, to re-sync entity display text
+   */
+  void setEntityDisplay(@NotNull Entity entity, @NotNull Player viewer, @Nullable Component text);
 
   Registry<SignRenderer> getSignRenderers();
 
