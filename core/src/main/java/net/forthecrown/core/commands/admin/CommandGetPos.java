3766cabd2828598bf6e3a87966446d10836bb30c
BotulToxin <73311304+JulieWoolie@users.noreply.github.com>
2023-11-18 15:31:49 +0200

    Add some tools to `/getpos`

--- core/src/main/java/net/forthecrown/core/commands/admin/CommandGetPos.java
+++ core/src/main/java/net/forthecrown/core/commands/admin/CommandGetPos.java
@@ -1,14 +1,24 @@
 package net.forthecrown.core.commands.admin;
 
+import static net.kyori.adventure.text.Component.text;
+
 import net.forthecrown.command.FtcCommand;
 import net.forthecrown.command.arguments.Arguments;
 import net.forthecrown.grenadier.GrenadierCommand;
 import net.forthecrown.text.Text;
 import net.forthecrown.user.User;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.event.ClickEvent;
 import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Location;
 
 public class CommandGetPos extends FtcCommand {
+
+  static final String JAVA_FORMAT
+      = "new Location(Bukkit.getWorld(\"%s\"), %sd, %sd, %sd, %sf, %sf)";
+
+  static final String TP_EXACT_FORMAT
+      = "/tp_exact world=%s x=%s y=%s z=%s yaw=%s pitch=%s";
 
   public CommandGetPos() {
     super("getpos");
@@ -24,6 +34,16 @@ public class CommandGetPos extends FtcCommand {
               User user = Arguments.getUser(c, "user");
               Location l = user.getLocation();
 
+              String clickJava = formatLocationVariables(JAVA_FORMAT, l, false);
+              String clickJavaRounded = formatLocationVariables(JAVA_FORMAT, l, true);
+              String clickCmd = formatLocationVariables(TP_EXACT_FORMAT, l, false);
+              String clickCmdRounded = formatLocationVariables(TP_EXACT_FORMAT, l, true);
+
+              Component tpExactRounded = toolButton("/tp_exact; rounded", clickCmdRounded);
+              Component tpExact = toolButton("/tp_exact", clickCmd);
+              Component javaConstructor = toolButton("Java", clickJava);
+              Component javaConstructorRounded = toolButton("Java; rounded", clickJavaRounded);
+
               c.getSource().sendMessage(
                   Text.format(
                       """
@@ -33,7 +53,8 @@ public class CommandGetPos extends FtcCommand {
                       z: &e{3}&r
                       yaw: &e{4}&r
                       pitch: &e{5}&r
-                      world: '&e{6}&r'""",
+                      world: '&e{6}&r'
+                      tools: {7} {8} {9} {10}""",
 
                       NamedTextColor.GRAY,
 
@@ -41,12 +62,62 @@ public class CommandGetPos extends FtcCommand {
                       l.getX(), l.getY(), l.getZ(),
                       l.getYaw(),
                       l.getPitch(),
-                      l.getWorld().getName()
+                      l.getWorld().getName(),
+
+                      tpExact,
+                      tpExactRounded,
+                      javaConstructor,
+                      javaConstructorRounded
                   )
               );
 
               return 0;
             })
         );
+  }
+
+  private static Component toolButton(String display, String copyText) {
+    return text()
+        .content("[" + display + "]")
+        .color(NamedTextColor.AQUA)
+
+        .clickEvent(ClickEvent.suggestCommand(copyText))
+        .insertion(copyText)
+        .hoverEvent(
+            text()
+                .content("Chat insertion: ")
+                .color(NamedTextColor.GRAY)
+                .append(text(copyText, NamedTextColor.WHITE))
+                .build()
+        )
+
+        .build();
+  }
+
+  private static String formatLocationVariables(String format, Location l, boolean round) {
+    return String.format(format,
+        l.getWorld().getName(),
+        round ? Math.floor(l.getX()) + 0.5D : l.getX(),
+        l.getY(),
+        round ? Math.floor(l.getZ()) + 0.5D : l.getZ(),
+        round ? roundRotation(l.getYaw()) : l.getYaw(),
+        round ? roundRotation(l.getPitch()) : l.getPitch()
+    );
+  }
+
+  private static float roundRotation(float value) {
+    float[] roundValues = {-180, -135, -90, -45, 0, 45, 90, 135, 180};
+    float roundRange = 45.0f / 2;
+
+    for (float roundValue : roundValues) {
+      float min = roundValue - roundRange;
+      float max = roundValue + roundRange;
+
+      if (value >= min && value <= max) {
+        return roundValue;
+      }
+    }
+
+    return value;
   }
 }
