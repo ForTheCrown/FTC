ea03d0b550e97b7a2a183684d50c1a18b30158da
BotulToxin <73311304+JulieWoolie@users.noreply.github.com>
2023-12-01 15:53:51 +0200

    Temporary fix for leaderboards not being shown when teleporting

--- leaderboards/src/main/java/net/forthecrown/leaderboards/listeners/PlayerListener.java
+++ leaderboards/src/main/java/net/forthecrown/leaderboards/listeners/PlayerListener.java
@@ -1,11 +1,14 @@
 package net.forthecrown.leaderboards.listeners;
 
+import com.destroystokyo.paper.event.player.PlayerTeleportEndGatewayEvent;
 import lombok.RequiredArgsConstructor;
 import net.forthecrown.leaderboards.LeaderboardPlugin;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
 
 @RequiredArgsConstructor
 public class PlayerListener implements Listener {
@@ -19,5 +22,19 @@ public class PlayerListener implements Listener {
     player.getScheduler().runDelayed(plugin, scheduledTask -> {
       plugin.getService().getTriggers().updateFor(player);
     }, null, 20);
+  }
+
+  @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
+  public void onPlayerTeleport(PlayerTeleportEvent event) {
+    Player player = event.getPlayer();
+
+    player.getScheduler().runDelayed(plugin, scheduledTask -> {
+      plugin.getService().getTriggers().updateFor(player);
+    }, null, 20);
+  }
+
+  @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
+  public void onPlayerTeleportEndGateway(PlayerTeleportEndGatewayEvent event) {
+    onPlayerTeleport(event);
   }
 }
