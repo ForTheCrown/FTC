2fdf6a38919a475286b38a540aa0b0741cae8a5f
BotulToxin <73311304+JulieWoolie@users.noreply.github.com>
2023-12-01 15:59:10 +0200

    Add `targets=<target selector>` argument to `/tp_exact`

--- core/src/main/java/net/forthecrown/core/commands/admin/CommandTeleportExact.java
+++ core/src/main/java/net/forthecrown/core/commands/admin/CommandTeleportExact.java
@@ -2,11 +2,15 @@ package net.forthecrown.core.commands.admin;
 
 import com.mojang.brigadier.arguments.DoubleArgumentType;
 import com.mojang.brigadier.arguments.FloatArgumentType;
+import java.util.Collection;
+import java.util.List;
 import net.forthecrown.command.FtcCommand;
+import net.forthecrown.command.arguments.ExpandedEntityArgument;
 import net.forthecrown.command.help.UsageFactory;
 import net.forthecrown.core.CorePermissions;
 import net.forthecrown.grenadier.GrenadierCommand;
 import net.forthecrown.grenadier.types.ArgumentTypes;
+import net.forthecrown.grenadier.types.EntitySelector;
 import net.forthecrown.grenadier.types.options.ArgumentOption;
 import net.forthecrown.grenadier.types.options.Options;
 import net.forthecrown.grenadier.types.options.OptionsArgument;
@@ -24,10 +28,17 @@ public class CommandTeleportExact extends FtcCommand {
   public static final ArgumentOption<World> WORLD
       = Options.argument(ArgumentTypes.world(), "world");
 
-  public static final ArgumentOption<Double>
-      CORD_X = Options.argument(DoubleArgumentType.doubleArg(), "x"),
-      CORD_Y = Options.argument(DoubleArgumentType.doubleArg(), "y"),
-      CORD_Z = Options.argument(DoubleArgumentType.doubleArg(), "z");
+  public static final ArgumentOption<Double> CORD_X
+      = Options.argument(DoubleArgumentType.doubleArg(), "x");
+
+  public static final ArgumentOption<Double> CORD_Y
+      = Options.argument(DoubleArgumentType.doubleArg(), "y");
+
+  public static final ArgumentOption<Double> CORD_Z
+      = Options.argument(DoubleArgumentType.doubleArg(), "z");
+
+  public static final ArgumentOption<EntitySelector> TARGETS
+      = Options.argument(new ExpandedEntityArgument(true, false), "targets");
 
   public static final ArgumentOption<Float> YAW
       = Options.argument(FloatArgumentType.floatArg(-180, 180))
@@ -49,6 +60,8 @@ public class CommandTeleportExact extends FtcCommand {
 
       .addOptional(YAW)
       .addOptional(PITCH)
+
+      .addOptional(TARGETS)
 
       .build();
 
@@ -84,16 +97,12 @@ public class CommandTeleportExact extends FtcCommand {
     return ClickEvent.runCommand(click);
   }
 
-  private static String formatCord(double pos) {
-    return String.format("%.02f", pos);
-  }
-
   @Override
   public void createCommand(GrenadierCommand command) {
     command
         .then(argument("args", ARGS)
             .executes(c -> {
-              var entity = c.getSource().as(Entity.class);
+              var source = c.getSource();
               var args = c.getArgument("args", ParsedOptions.class);
 
               Location loc = new Location(
@@ -105,20 +114,30 @@ public class CommandTeleportExact extends FtcCommand {
                   args.getValue(PITCH)
               );
 
-              if (entity instanceof Player player) {
-                var user = Users.get(player);
+              Collection<Entity> targets;
 
-                if (!user.checkTeleporting()) {
-                  return 0;
-                }
-
-                user.createTeleport(() -> loc, UserTeleport.Type.TELEPORT)
-                    .setDelay(null)
-                    .setSilent(true)
-                    .start();
-
+              if (args.has(TARGETS)) {
+                targets = args.getValue(TARGETS).findEntities(source);
               } else {
-                entity.teleport(loc);
+                targets = List.of(source.asEntity());
+              }
+
+              for (Entity target : targets) {
+                if (target instanceof Player player) {
+                  var user = Users.get(player);
+
+                  if (!user.checkTeleporting()) {
+                    return 0;
+                  }
+
+                  user.createTeleport(() -> loc, UserTeleport.Type.TELEPORT)
+                      .setDelay(null)
+                      .setSilent(true)
+                      .start();
+
+                } else {
+                  target.teleport(loc);
+                }
               }
 
               return 0;
